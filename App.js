4257425fc71d826142cf7c811f056cda6a046020
Akshit Gupta <akshitgupta1996@gmail.com>
2018-10-19 22:12:48 -0700

    ND019 C3 L2-22 - AddEntry-UdaciSlider

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View, Text, Slider} from 'react-native';
 
 import AddEntry from './components/AddEntry'
 
 export default class App extends React.Component {
+  state = {
+    value: 0
+  }
   componentDidMount(){}
   render() {
     return (
       <View>
-        <Text>-</Text>
         <Text>-</Text>
         <Text>-</Text>
         <AddEntry />
