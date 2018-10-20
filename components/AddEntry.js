f4f6811fe3f313098bfc8116dc71f081667522a3
Akshit Gupta <akshitgupta1996@gmail.com>
2018-10-19 22:03:43 -0700

    ND019 C3 L2-19 - AddEntry-SubmitBtn

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native'
-import {getMetricMetaInfo} from '../utils/helpers'
+import {View, Text, TouchableOpacity} from 'react-native'
+import {getMetricMetaInfo, timeToString} from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+
+function SumbitBtn ({ onPress }) {
+    return (
+        <TouchableOpacity 
+         onPress={onPress}>
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
 
 
 export default class AddEntry extends Component {
@@ -46,6 +55,25 @@ export default class AddEntry extends Component {
         }))
     }
 
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+
+        // Update Redux
+
+        this.setState(() => ({
+            run: 0,
+            bike: 0,
+            swim: 0,
+            sleep: 0,
+            eat: 0,
+        }))
+
+        // Navigate Home
+
+        // Clear local notifcation
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo()
 
@@ -76,6 +104,8 @@ export default class AddEntry extends Component {
                         </View>
                     )
                 })}
+
+                <SumbitBtn onPress={this.submit}/> 
 
             </View>
         )
