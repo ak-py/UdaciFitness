3fada4c3e05b2e4d74ebe5fd59631a2da8a41879
Akshit Gupta <akshitgupta1996@gmail.com>
2018-10-21 05:28:20 -0700

    Style-Rest

--- components/TextButton.js
+++ components/TextButton.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import {FontAwesome, Entypo} from '@expo/vector-icons'
+import { purple } from '../utils/colors';
 
-export default function TextButton ({ children, onPress }) {
+export default function TextButton ({ children, onPress, style = {} }) {
     return (
         <TouchableOpacity onPress={onPress}>
-            <Text>{children}</Text>
+            <Text style={[styles.reset, style]} >{children}</Text>
         </TouchableOpacity>
     )
 }
+
+const styles = StyleSheet.create({
+    reset: {
+        textAlign: 'center',
+        color: purple,
+    }
+})
--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,20 +1,35 @@
 import React from 'react'
-import { View, Slider, Text } from 'react-native'
+import { View, Slider, Text, StyleSheet, Platform } from 'react-native'
+import { gray } from '../utils/colors';
 
 export default function UdaciSlider ({ max, unit, step, value, onChange }) {
     return (
-        <View>
+        <View style={styles.rowin}>
             <Slider
+                style={{flex: 1}}
                 step = {step}
                 value = {value}
                 maximumValue = {max}
                 minimumValue = {0}
                 onValueChange = {onChange}
             />
-            <View>
-                <Text>{value}</Text>
-                <Text>{unit}</Text>
+            <View stlye={styles.metricCounter} >
+                <Text style={{fontSize: 24, textAlign:'center'}} >{value}</Text>
+                <Text stlye={{fontSize: 18, color: gray}}>{unit}</Text>
             </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    rowin: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center'
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
